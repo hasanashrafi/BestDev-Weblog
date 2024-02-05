d675705df32d40e4b9e5b72e8b854e25016a29bb
Hasan Ashrafi <yasashrafi@gmail.com>
2024-02-05 10:50:47 +0330

    added header component to app

--- bestdev-blog/src/App.js
+++ bestdev-blog/src/App.js
@@ -1,22 +1,11 @@
-import { gql, useQuery } from '@apollo/client';
-import './App.css';
 import Header from './components/layout/Header';
-
-// const QUERY = gql`
-// query{
-//   posts{content}
-  
-// }
-
-// `
 
 
 function App() {
-  // const { loading, data } = useQuery(QUERY)
-  // console.log(data);
+
   return (
     <div>
-    <Header/>
+      <Header />
     </div>
   );
 }
