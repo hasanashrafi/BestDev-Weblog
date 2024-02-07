7dc930ebcd14c40d20bc0cfc53910390f245c4c3
Hasan Ashrafi <yasashrafi@gmail.com>
2024-02-07 22:17:15 +0330

    create card component and design ui with mui

---
+++ bestdev-blog/src/components/shared/CardEl.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Divider, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+function CardEl({ title, slug, coverPhoto, author }) {
+    return (
+        <Card sx={{ boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px", borderRadius: "4px" }}  >
+            <CardHeader
+                avatar={
+                    <Avatar src={author.avatar.url} sx={{ marginLeft: 3 }} />}
+                title={
+                    <Typography
+                        component="p"
+                        variant="p"
+                        color="text.secondary">
+                        {author.name}
+                    </Typography>} />
+
+            <CardMedia component="img" height="194" width="100%" image={coverPhoto.url} alt={slug} />
+
+            <CardContent>
+                <Typography
+                    component="h3"
+                    variant="h6"
+                    color="text.primary">
+                    {title}
+                </Typography>
+            </CardContent>
+
+            <Divider variant="middle" sx={{ margin: "10px" }} />
+
+            <CardActions>
+                <Link to={`/blogs/${slug}`} style={{textDecoration:"none", width:"100%"}}>
+                <Button
+                    variant="outlined"
+                    size="small"
+                    sx={{ width: "100%" }}>
+                    مطالعه مقاله
+                </Button>
+                </Link>
+            </CardActions>
+
+        </Card>
+    );
+}
+
+export default CardEl;
